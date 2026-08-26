9821c48bcc4e15dfe4bae21f50e1bdcfe419781e
ZJN2025 <779661066@qq.com>
2026-08-27 01:00:09 +0800

    gemm_shred_tiling relized

--- kernels/gemm_shared_tiling.cu
+++ kernels/gemm_shared_tiling.cu
@@ -17,21 +17,36 @@ __global__ void gemm_shared_tiling(
     __shared__ float tile_A[TILE_SIZE][TILE_SIZE];
     __shared__ float tile_B[TILE_SIZE][TILE_SIZE];
 
-    const int row=blockIdx.y*TILE_SIZE+threadIdx.y;
-    const int col=blockIdx,x*TILE_SIZE+threadIdx.x;
+    const int tx=threadIdx.x;
+    const int ty=threadIdx.y;
+
+    const int row=blockIdx.y*TILE_SIZE+ty;
+    const int col=blockIdx,x*TILE_SIZE+tx;
 
     float sum=0.0f;
     //沿K维切分tile
     for(int tile_k=0;tile_k<K;tile_k+=TILE_SIZE){
-        //loda data to shared memory
+        if(row<M&&tile_k+tx<K){
+            tile_A[ty][tx]=A[row*K+tile_k+tx];
+        }else{
+            tile_A[ty][tx]=0.0f;
+        }
 
+        //loda data to shared memory
+        if(col<N&&tile_k+ty<K){
+            tile_B[ty][tx]=B[(tile_k + ty) * N +
+                  col];
+        }else{
+            tile_B[ty][tx]=0.0f;
+        }
 
         __syncthreads();
 
         //内积
         for(int k=0;k<TILE_SIZE;++k){
-
-
+            sum +=
+                tile_A[ty][k] *
+                tile_B[k][tx];
         }
 
         __syncthreads();
@@ -42,18 +57,10 @@ __global__ void gemm_shared_tiling(
 
 
     }
+    if (row < M && col < N) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        C[row * N + col] =
+            alpha * sum +
+            beta * C[row * N + col];
+    }
 }
